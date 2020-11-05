c04d67c2bba5e25bab95c5800b7b74b067dad73c
Aaron <aaron.johnson35043@gmail.com>
2020-11-05 09:32:48 -0600

    Add project files.

---
+++ ConsoleApp12/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        private double balance = 0;
+        public double Balance;
+        
+            public void Deposit(double amount)
+            {
+            balance = amount;
+            }
+            public double GetBalance()
+            {
+            return balance;
+             }
+     }
+    
+}
